b1c611e365a7059573e81ff508d258374b9b74df
kosuke55 <kosuke.tnp@gmail.com>
2020-10-08 06:00:51 +0900

    Update shapenet synset

--- kaolin/datasets/shapenet.py
+++ kaolin/datasets/shapenet.py
@@ -45,22 +45,7 @@ import kaolin.conversions.meshconversions as mesh_cvt
 
 from .base import KaolinDataset
 
-# Synset to Label mapping (for ShapeNet core classes)
-synset_to_label = {'04379243': 'table', '03211117': 'monitor', '04401088': 'phone',
-                   '04530566': 'watercraft', '03001627': 'chair', '03636649': 'lamp',
-                   '03691459': 'speaker', '02828884': 'bench', '02691156': 'plane',
-                   '02808440': 'bathtub', '02871439': 'bookcase', '02773838': 'bag',
-                   '02801938': 'basket', '02880940': 'bowl', '02924116': 'bus',
-                   '02933112': 'cabinet', '02942699': 'camera', '02958343': 'car',
-                   '03207941': 'dishwasher', '03337140': 'file', '03624134': 'knife',
-                   '03642806': 'laptop', '03710193': 'mailbox', '03761084': 'microwave',
-                   '03928116': 'piano', '03938244': 'pillow', '03948459': 'pistol',
-                   '04004475': 'printer', '04099429': 'rocket', '04256520': 'sofa',
-                   '04554684': 'washer', '04090263': 'rifle', '02946921': 'can',
-                   '03797390': 'mug'}
-
-# Label to Synset mapping (for ShapeNet core classes)
-label_to_synset = {v: k for k, v in synset_to_label.items()}
+from shapenet_utils import synset_to_label, label_to_synset
 
 
 class print_wrapper(object):
